0b45db9c7dd401c6d577fd260344c9cf6b85db81
Kim Jeong-won <threedalpeng@gmail.com>
2022-04-24 14:34:15 +0900

    Add a global prefix

--- webgl-guide-backend/src/main.ts
+++ webgl-guide-backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8081);
+  app.setGlobalPrefix('api');
+  await app.listen(3010);
 }
 bootstrap();
