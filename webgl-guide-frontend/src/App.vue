da9afe749a505d56ffff9a76420999e6ad03802b
Kim Jeong-won <threedalpeng@gmail.com>
2022-04-30 18:23:26 +0900

    Delete header text for webgl-guide

--- webgl-guide-frontend/src/App.vue
+++ webgl-guide-frontend/src/App.vue
@@ -1,17 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex flex-col items-center gap-8">
-    <div>
-      <h1>WebGL Study</h1>
-    </div>
-    <Suspense>
-      <template #default>
-        <router-view></router-view>
-      </template>
-      <template #fallback> loading... </template>
-    </Suspense>
-  </div>
+  <Suspense>
+    <template #default>
+      <router-view></router-view>
+    </template>
+    <template #fallback> loading... </template>
+  </Suspense>
 </template>
 
 <style>
