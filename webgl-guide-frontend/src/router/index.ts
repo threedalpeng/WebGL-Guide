1c71eaa3dd719d1122414e9be2b0efc0fcdaea61
Kim Jeong-won <threedalpeng@gmail.com>
2022-04-30 18:22:47 +0900

    Rename Home things to Main

--- webgl-guide-frontend/src/router/index.ts
+++ webgl-guide-frontend/src/router/index.ts
@@ -1,11 +1,11 @@
-import HomePage from "../page/home/HomePage.vue";
+import MainPage from "../page/main/MainPage.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const LecturePage = () => import("../page/lecture/LecturePage.vue");
 const DemoPage = () => import("../page/demo/DemoPage.vue");
 
 const routes = [
-  { path: "/", component: HomePage },
+  { path: "/", component: MainPage },
   { path: "/lecture/:id", component: LecturePage },
   { path: "/demo", component: DemoPage },
 ];
