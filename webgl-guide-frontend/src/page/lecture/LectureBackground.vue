30612060109df3902328492614d0e87a5437e1d4
Kim Jeong-won <threedalpeng@gmail.com>
2022-06-09 15:43:47 +0900

    Attach a iframe to background

---
+++ webgl-guide-frontend/src/page/lecture/LectureBackground.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <iframe class="background-demo" src="/demo/pong" tabindex="0"></iframe>
+</template>
+
+<style scoped>
+.background-demo {
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  left: 0;
+  top: 0;
+  border: none;
+  z-index: -1;
+}
+</style>
