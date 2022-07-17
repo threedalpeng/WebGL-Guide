af24550ed99b714e99cd0f5fd9f4b48a6ef8257d
Kim Jeong-won <threedalpeng@gmail.com>
2022-07-17 23:29:59 +0900

    Now background demo is fixed to top and focusable

--- webgl-guide-frontend/src/page/lecture/LectureBackground.vue
+++ webgl-guide-frontend/src/page/lecture/LectureBackground.vue
@@ -1,17 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const focusOnClick = (e: PointerEvent) => {
+  console.log("pointerdown");
+  (e.target as HTMLIFrameElement).focus();
+};
+</script>
 
 <template>
-  <iframe class="background-demo" src="/demo/pong" tabindex="0"></iframe>
+  <iframe
+    class="background-demo"
+    src="/demo/pong"
+    tabindex="0"
+    @pointerdown="focusOnClick"
+  ></iframe>
 </template>
 
 <style scoped>
 .background-demo {
-  position: fixed;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+
   width: 100vw;
   height: 100vh;
-  left: 0;
-  top: 0;
   border: none;
-  z-index: -1;
 }
 </style>
